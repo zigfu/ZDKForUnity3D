7e67192bb9ba8430f87fa854ee6b6cb4779b0fb7
Shlomo Zippel <shlomo@zigfu.com>
2011-08-21 19:02:16 -0700

    updates to docs

--- doc/makedocs.bat
+++ doc/makedocs.bat
@@ -1,7 +1,5 @@
 @echo off
 call mdoc update -o xml -L "c:\Program Files\Unity\Editor\Data\Managed" ..\Library\ScriptAssemblies\Assembly-CSharp.dll
 docfixinator.py xml
-echo "Make changes to XML files in [out] and press any key when done"
-pause
 call mdoc export-html --template doctemplate.xsl -o html out
 
