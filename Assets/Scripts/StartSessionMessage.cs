0b1d2f4b5dec6dbb010a5b3e41d85685a588a844
Shlomo Zippel <shlomo@zigfu.com>
2011-08-27 00:54:43 -0700

    item selector (static menu) sample scene

---
+++ Assets/Scripts/StartSessionMessage.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class StartSessionMessage : MonoBehaviour {
+    void OnGUI()
+    {
+        if (!SessionManager.InSession) {
+            GUILayout.BeginArea(new Rect(Screen.width / 2 - 150, 50, 300, 300));
+            GUILayout.Box("Please perform a focus gesture to start the session");
+            GUILayout.EndArea();
+        }
+    }
+}
