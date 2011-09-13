4b7986c513ff812a5f911794156adde967d22b29
shlomo.zippel <shlomo@zigfu.com>
2011-09-13 15:52:15 -0700

    slight changes to scene builder

--- Assets/Editor/BuildScenes.cs
+++ Assets/Editor/BuildScenes.cs
@@ -1,15 +1,20 @@
 using UnityEditor;
 using UnityEngine;
+using System.IO;
 using System.Collections;
 
 public class BuildScenes {
     static string scenesPath = "Assets/OpenNI/Scenes/";
-    static string outputPath = "d:\\work\\test";
+    static string outputPath = "Demos";
 
 
-    [MenuItem("Build/BuildSceneExecutables")]
+    [MenuItem("Build/Build Scene Executables")]
     static void Build()
     {   
+		if (!Directory.Exists(outputPath)) {
+			Directory.CreateDirectory(outputPath);
+		}
+		
         string[] scenes = {"Avatar2Players", "AvatarFrontFacing"};
         foreach(string scene in scenes) {
             string res = BuildPipeline.BuildPlayer(new string[] { scenesPath + scene + ".unity" }, getOutputPath(scene), BuildTarget.StandaloneWindows, BuildOptions.None);
