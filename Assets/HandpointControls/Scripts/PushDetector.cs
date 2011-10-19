8e86a7667288c57769a6604631b8040822fb1cff
shlomo.zippel <shlomo@zigfu.com>
2011-10-18 18:31:53 -0700

    Send PushDetector_Push only when we are sure its not a click

--- Assets/HandpointControls/Scripts/PushDetector.cs
+++ Assets/HandpointControls/Scripts/PushDetector.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class PushDetector : MonoBehaviour {
 	public float size = 150.0f;
 	public float initialValue = 0.2f;
-	public float driftSpeed = 1.5f;
+	public float driftSpeed = 0.05f;
 	
 	public float clickTimeFrame = 1.0f;
     public float clickMaxDistance = 100; //??
@@ -36,6 +36,7 @@ public class PushDetector : MonoBehaviour {
 		Hand_Update(pos);
 	}
 	
+	bool sent_push;
 	void Hand_Update(Vector3 pos)
 	{
 		// move slider if hand is out of its bounds (that way it always feels responsive)
@@ -49,35 +50,49 @@ public class PushDetector : MonoBehaviour {
 		// click logic
 		if (!IsClicked)
         {
-            if (ClickProgress == 1.0f)
-            {
+            if (ClickProgress == 1.0f) {
 				ClickPosition = pos;
                 IsClicked = true;
 				clickPushTime = Time.time;
-				SendMessage("PushDetector_Push", SendMessageOptions.DontRequireReceiver);
+				//SendMessage("PushDetector_Push", SendMessageOptions.DontRequireReceiver);
             }
         }
         else // clicked
         {
-            if (ClickProgress < 0.5)
-            {
+            if (ClickProgress < 0.5) {
                 if (IsClick(clickPushTime, ClickPosition, Time.time, pos)) {
 					SendMessage("PushDetector_Click",SendMessageOptions.DontRequireReceiver);
 				}
 				
 				SendMessage("PushDetector_Release", SendMessageOptions.DontRequireReceiver);
                 IsClicked = false;
+				sent_push = false;
             }
+			else
+			{
+				if (!sent_push && !IsClick(clickPushTime, ClickPosition, Time.time, pos))
+				{
+					SendMessage("PushDetector_Push", SendMessageOptions.DontRequireReceiver);
+					sent_push = true;
+				}
+			}
         }
 		
 		// drift the slider to the initial position, if we aren't clicked
 		if (!IsClicked) {
 			float delta = initialValue - ClickProgress;
-            //pushFader.MoveTo(pos, ClickProgress + delta 
-            pushFader.MoveTo(pos, ClickProgress + (delta * 0.02f));
+            pushFader.MoveTo(pos, ClickProgress + (delta * driftSpeed));
 		}	
 	}
-
+	
+	void Hand_Destroy()
+	{
+		if (IsClicked) {
+			SendMessage("PushDetector_Release", SendMessageOptions.DontRequireReceiver);
+			IsClicked = false;
+		}
+	}
+	
     bool IsClick(float t1, Vector3 p1, float t2, Vector3 p2)
     {
         Vector3 delta = (p2 - p1);
@@ -91,12 +106,5 @@ public class PushDetector : MonoBehaviour {
 		GUILayout.Toggle(IsClicked, "PUSH");
 	}
 	
-	void Hand_Destroy()
-	{
-		if (IsClicked)
-		{
-			SendMessage("PushDetector_Release", SendMessageOptions.DontRequireReceiver);
-			IsClicked = false;
-		}
-	}
+
 }
