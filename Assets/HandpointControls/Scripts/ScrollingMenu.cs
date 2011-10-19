962f8b1b9c335f8ef2b8a6380630a7a1fdd5beb6
shlomo.zippel <shlomo@zigfu.com>
2011-10-18 18:32:57 -0700

    added push sliding, out of bounds

--- Assets/HandpointControls/Scripts/ScrollingMenu.cs
+++ Assets/HandpointControls/Scripts/ScrollingMenu.cs
@@ -5,18 +5,26 @@ using System.Collections.Generic;
 [RequireComponent(typeof(Fader))]
 public class ScrollingMenu : MonoBehaviour
 {
-    public bool SelectOnPush;
     public Fader fader;
     public Vector3 direction = Vector3.down;
     public int WindowSize;
     public float damping = 5.0f;
-    public bool RepositionBasedOnBounds = false;
     public float scrollRegionSize = 0.15f;
-
+    public bool RepositionBasedOnBounds = false;
+	public bool RepositionItemsOnUpdate;
+    public bool SelectOnPush;
+	public bool PushSlide;
+	public float pushSliderSize = 200;
+	public float pushSliderSensitiviy = 10.0f;
+	
     List<Transform> items = new List<Transform>();
     ItemSelector selector;
     GameObject itemsContainer;
     Vector3 itemsContainerTargetPos;
+	
+	Fader pushslideFader;
+	bool pushSliding;
+	float centerIndexBase;
 
     float centerIndex = 0;
     public float CenterIndex
@@ -74,7 +82,12 @@ public class ScrollingMenu : MonoBehaviour
                 Unhighlight();
                 HighlightItem(clamped);
             }
-
+			
+			// OOB
+			if (clamped != value) {
+				SendMessage("Menu_OutOfBounds", (value > 0), SendMessageOptions.DontRequireReceiver);
+			}
+			
             // see if we should scroll our sliding window
             if (activeItemIndex < firstOnScreenIndex) {
                 firstOnScreenIndex = activeItemIndex;
@@ -128,20 +141,49 @@ public class ScrollingMenu : MonoBehaviour
         selector.fader = fader;
     
         // push detector, if we need one
-        if (SelectOnPush) {
+        if (SelectOnPush || PushSlide) {
             if (null == GetComponent<PushDetector>()) {
                 gameObject.AddComponent<PushDetector>();
             }
         }
+		
+		// fader for the pushslider, if we need one
+		if (PushSlide) {
+			pushslideFader = gameObject.AddComponent<Fader>() as Fader;
+			pushslideFader.direction = fader.direction;
+			pushslideFader.size = pushSliderSize;
+		}
     }
 
    	// Update is called once per frame
     void Update()
     {
-        // move itemscontainer
-        itemsContainer.transform.localPosition = Vector3.Lerp(itemsContainer.transform.localPosition, itemsContainerTargetPos, Time.deltaTime * damping);
+		// push sliding
+		if (pushSliding) {
+			// convert 0-1 slider to -1 to 1 slider
+			float pushedOffset = (pushslideFader.value - 0.5f) * 2;
+			CenterIndex = centerIndexBase + (pushedOffset * pushSliderSensitiviy);
+		}
+		
+		// move itemscontainer
+       	itemsContainer.transform.localPosition = Vector3.Lerp(itemsContainer.transform.localPosition, itemsContainerTargetPos, Time.deltaTime * damping);
+		
+		if (RepositionItemsOnUpdate) {
+			RepositionItems();
+		}
     }
-
+	
+	// Backwards compatibility
+	void Menu_Add(Transform item)
+	{
+		Add(item);
+	}
+	
+	void Menu_Reposition()
+	{
+		RepositionItems();
+	}
+	
     public void Add(Transform item)
     {
         item.parent = itemsContainer.transform;
@@ -215,16 +257,19 @@ public class ScrollingMenu : MonoBehaviour
 
     void ItemSelector_Select(int index)
     {
+		if (pushSliding) return;
         ActiveItemIndex = firstOnScreenIndex + index;
     }
 
     void ItemSelector_Next()
     {
+		if (pushSliding) return;
         ActiveItemIndex++;
     }
 
     void ItemSelector_Prev()
     {
+		if (pushSliding) return;
         ActiveItemIndex--;
     }
 
@@ -232,6 +277,23 @@ public class ScrollingMenu : MonoBehaviour
     // PushDetector messages
     //-------------------------------------------------------------------------
 
+	void PushDetector_Push()
+	{
+		if (PushSlide) {
+			pushslideFader.MoveTo(gameObject.GetComponent<PushDetector>().ClickPosition, 0.5f);
+			centerIndexBase = CenterIndex;
+			pushSliding = true;
+		}
+	}
+	
+	void PushDetector_Release()
+	{
+		if (PushSlide) {
+			pushSliding = false;
+			ActiveItemIndex = Mathf.FloorToInt(CenterIndex);
+		}
+	}
+	
     void PushDetector_Click()
     {
         if (SelectOnPush) {
