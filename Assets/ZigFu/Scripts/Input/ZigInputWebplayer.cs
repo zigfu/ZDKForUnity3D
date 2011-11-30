b152538c12575b52733c7fd4dd9ea88a48407ef2
Roee Shenberg <roee@zigfu.com>
2011-11-29 20:53:35 -0800

    the webplayer input now injects the plugin to the containing page.

--- Assets/ZigFu/Scripts/Input/ZigInputWebplayer.cs
+++ Assets/ZigFu/Scripts/Input/ZigInputWebplayer.cs
@@ -1,26 +1,79 @@
 using UnityEngine;
 using System.Collections;
 
+#region Javascript
+class WebplayerJavascript
+{
+    // WARNING - change only if you know what you doing (take off every zig)
+    public const string getPlugin = @"function getZigPlugin() {
+		return document.getElementById('zigPluginObject');
+	}
+    ";
+
+    public const string addHandler = @"function XBrowserAddHandler(target,eventName,handlerName)
+	{
+		if ( target.attachEvent )
+			target.attachEvent('on' + eventName, handlerName);
+		else if ( target.addEventListener )
+			target.addEventListener(eventName, handlerName, false);
+		else
+			target['on' + eventName] = handlerName;
+	}
+    ";
+
+    public const string pluginLoaded = @"function zigPluginLoaded() {
+		XBrowserAddHandler(getZigPlugin(), 'NewFrame', onZigNewFrame);
+        }
+    ";
+
+    public const string onUpdate = @"function onZigNewFrame()
+	{{
+		var plug = getZigPlugin();
+		var unity = unityObject.getObjectById('unityPlayer');
+		unity.SendMessage('{0}', 'NewData', JSON.stringify([plug.users, plug.hands]));
+	}}
+    ";
+    public const string injectPlugin = @"function injectPlugin(callback) {
+		zigPluginLoadedCB = callback;" +
+        "\nvar html = '<object id=\"zigPluginObject\" type=\"application/x-zig\" width=\"0\" height=\"0\">';\n" +
+        "html += '<param name=\"onload\" value=\"zigPluginLoadedCB\" />';\n" +
+        @"html += '</object>';
+		var newDiv = document.createElement('div');
+		newDiv.innerHTML = html;
+		document.body.appendChild(newDiv);
+	}
+    ";
+
+    static public string getInjectJS(string listenerName)
+    {
+        return WebplayerJavascript.getPlugin
+                + WebplayerJavascript.addHandler
+                + WebplayerJavascript.pluginLoaded
+                + string.Format(WebplayerJavascript.onUpdate, listenerName)
+                + WebplayerJavascript.injectPlugin
+                + "injectPlugin(zigPluginLoaded);\n";
+    }
+}
+
+#endregion
+
 public class ZigInputWebplayer : MonoBehaviour {
+
     public ZigUserTracker UserTracker;
+    public void doInjectPlugin() {
+        Application.ExternalEval(WebplayerJavascript.getInjectJS(gameObject.name));
+    }
 
-    public string ZigJSElementID = "ZigJS";
-
-
-    public GameObject MosheObject;
-    public GameObject NewDataObject;
 	// Use this for initialization
 	void Start () {
         if (!Application.isWebPlayer) {
             print("Only web player supported by ZigInputWebplayer!");
             return;
         }
+        else {
+            doInjectPlugin();
+        }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 
     // needed for some disparity between the output of the JSON decoder and our direct OpenNI layer
@@ -30,11 +83,9 @@ public class ZigInputWebplayer : MonoBehaviour {
             obj[property] = int.Parse(obj[property].ToString());
         }
     }
-
+    // called from javascript, giving us new data from the plugin
     void NewData(string param)
     {
-        NewDataObject.SetActiveRecursively(true);
-        Logger.Log("Got new data: " + param);
         try {
             ArrayList data = (ArrayList)JSON.JsonDecode(param);
             ArrayList users = (ArrayList)data[0];
@@ -45,7 +96,7 @@ public class ZigInputWebplayer : MonoBehaviour {
             UserTracker.UpdateData(users, hands);
         }
         catch (System.Exception ex) {
-            Logger.Log(ex.ToString());
+            Logger.Log(ex.ToString()); // the only we we'll actually see the exception
         }
     }
 }
