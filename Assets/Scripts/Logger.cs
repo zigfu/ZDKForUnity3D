0c3b2fb5591d888f9524dafe3649b9bd59130121
Roee Shenberg <roee@zigfu.com>
2011-11-29 18:46:55 -0800

    Initial working version of webplayer javascript input. For now it expects the page to feed it data. Will be changed to inject the plugin to the page later...

---
+++ Assets/Scripts/Logger.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Logger : MonoBehaviour {
+		// singleton
+    static Logger instance;
+    public static Logger Instance
+    {
+        get
+        {
+            if (null == instance) {
+                instance = FindObjectOfType(typeof(Logger)) as Logger;
+                if (null == instance) {
+                    GameObject container = new GameObject();
+                    DontDestroyOnLoad(container);
+                    container.name = "LoggerContainer";
+                    instance = container.AddComponent<Logger>();
+                }
+                DontDestroyOnLoad(instance);
+            }
+            return instance;
+        }
+    }
+
+	
+	public static void Log(string s)
+	{
+		Instance.log(s);
+	}
+	
+	public int maxItems = 10;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	List<string> logEntries = new List<string>();
+	
+	public void log(string str)
+	{
+		logEntries.Add(str);
+		if (logEntries.Count > maxItems) {
+			logEntries.RemoveRange(0, logEntries.Count - maxItems);
+		}
+	}
+	
+	void OnGUI()
+	{
+		GUILayout.Box("Log:");
+		foreach (string s in logEntries) {	
+			GUILayout.Label(s);
+		}
+	}
+}
--- Assets/ZigFu/Scripts/Input/ZigInputWebplayer.cs
+++ Assets/ZigFu/Scripts/Input/ZigInputWebplayer.cs
@@ -2,14 +2,50 @@ using UnityEngine;
 using System.Collections;
 
 public class ZigInputWebplayer : MonoBehaviour {
+    public ZigUserTracker UserTracker;
 
+    public string ZigJSElementID = "ZigJS";
+
+
+    public GameObject MosheObject;
+    public GameObject NewDataObject;
 	// Use this for initialization
 	void Start () {
-	
+        if (!Application.isWebPlayer) {
+            print("Only web player supported by ZigInputWebplayer!");
+            return;
+        }
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    // needed for some disparity between the output of the JSON decoder and our direct OpenNI layer
+    static void Intify(ArrayList list, string property)
+    {
+        foreach (Hashtable obj in list) {
+            obj[property] = int.Parse(obj[property].ToString());
+        }
+    }
+
+    void NewData(string param)
+    {
+        NewDataObject.SetActiveRecursively(true);
+        Logger.Log("Got new data: " + param);
+        try {
+            ArrayList data = (ArrayList)JSON.JsonDecode(param);
+            ArrayList users = (ArrayList)data[0];
+            ArrayList hands = (ArrayList)data[1];
+            Intify(users, "id");
+            Intify(hands, "id");
+            Intify(hands, "userid");
+            UserTracker.UpdateData(users, hands);
+        }
+        catch (System.Exception ex) {
+            Logger.Log(ex.ToString());
+        }
+    }
 }
