5c467be0271021954771992eb89fb003128b9710
shlomo.zippel <shlomo@zigfu.com>
2012-01-30 21:32:41 -0800

    YAY! depth from kinect sdk, and start/stop in unity

--- Assets/ZigFu/Scripts/Input/ZigInputKinectSDK.cs
+++ Assets/ZigFu/Scripts/Input/ZigInputKinectSDK.cs
@@ -29,9 +29,43 @@ class PreventDoubleInit
 	static extern IntPtr CreateEvent(IntPtr lpEventAttributes, bool bManualReset, bool bInitialState, string lpName);
 	[DllImport("kernel32.dll")]
 	static extern IntPtr OpenEvent(uint dwDesiredAccess, bool bInheritHandle, string lpName);
+    [DllImport("kernel32.dll")]
+    static extern IntPtr CreateFileMapping(IntPtr hFile, IntPtr lpFileMappingAttributes, FileMapProtection flProtect, uint dwMaximumSizeHigh, uint dwMaximumSizeLow, [MarshalAs(UnmanagedType.LPTStr)] string lpName);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern IntPtr MapViewOfFile(IntPtr hFileMappingObject, FileMapAccess dwDesiredAccess, uint dwFileOffsetHigh, uint dwFileOffsetLow, uint dwNumberOfBytesToMap);
+    [DllImport("Kernel32.dll")]
+    static extern bool UnmapViewOfFile(IntPtr map);
+    [DllImport("kernel32.dll")]
+    static extern int CloseHandle(IntPtr hObject);
+
+    enum FileMapProtection : uint
+    {
+        PageReadonly = 0x02,
+        PageReadWrite = 0x04,
+        PageWriteCopy = 0x08,
+        PageExecuteRead = 0x20,
+        PageExecuteReadWrite = 0x40,
+        SectionCommit = 0x8000000,
+        SectionImage = 0x1000000,
+        SectionNoCache = 0x10000000,
+        SectionReserve = 0x4000000,
+    }
+
+    [Flags]
+    public enum FileMapAccess : uint
+    {
+        FileMapCopy = 0x0001,
+        FileMapWrite = 0x0002,
+        FileMapRead = 0x0004,
+        FileMapAllAccess = 0x001f,
+        FileMapExecute = 0x0020,
+    }
+
+    static IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
 	
-	const string eventName = "KinectReader_PreventDoubleInit";
-	
+    const string eventName = "KinectReader_PreventDoubleInit";
+    const string memoryMapName = "KinectReader_PreventDoubleInitMemoryMap";
+
 	public static bool IsSafeToInit() {
 		return (IntPtr.Zero == OpenEvent(2, false, eventName));
 	}
@@ -39,6 +73,29 @@ class PreventDoubleInit
 	public static void MarkInited() {
 		CreateEvent(IntPtr.Zero, false, false, eventName);
 	}
+
+    public static void SaveContext<T>(T toSave) {
+        IntPtr h = CreateFileMapping(INVALID_HANDLE_VALUE, IntPtr.Zero, FileMapProtection.PageReadWrite, 0, (uint)Marshal.SizeOf(typeof(T)), memoryMapName);
+        IntPtr buffer = MapViewOfFile(h, FileMapAccess.FileMapWrite, 0, 0, (uint)Marshal.SizeOf(typeof(T)));
+        Marshal.StructureToPtr(toSave, buffer, false);
+        UnmapViewOfFile(buffer);
+        // No closehandle here!
+    }
+
+    public static T LoadContext<T>() {
+        IntPtr h = CreateFileMapping(INVALID_HANDLE_VALUE, IntPtr.Zero, FileMapProtection.PageReadWrite, 0, (uint)Marshal.SizeOf(typeof(T)), memoryMapName);
+        IntPtr buffer = MapViewOfFile(h, FileMapAccess.FileMapRead, 0, 0, (uint)Marshal.SizeOf(typeof(T)));
+        T result = (T) Marshal.PtrToStructure(buffer, typeof(T));
+        UnmapViewOfFile(buffer);
+        CloseHandle(h);
+        return result;
+    }
+}
+
+public struct NuiContext
+{
+    public IntPtr DepthHandle;
+    public IntPtr ImageHandle;
 }
 
 public class NuiWrapper
@@ -90,6 +147,24 @@ public class NuiWrapper
 		UsesSkeleton			= 0x00000008,
 		UsesDepth 				= 0x00000020,
 	}
+
+    public enum NuiImageType
+    {
+        DepthAndPlayerIndex = 0,
+        Color,
+        ColorYuv,
+        ColorRawYuv,
+        Depth
+    }
+
+    public enum NuiImageResolution
+    {
+        Invalid = -1,
+        Res80x60 = 0,
+        Res320x240,
+        Res640x480,
+        Res1280x960,
+    }
 	
 	[StructLayout(LayoutKind.Sequential)]
     public struct LARGE_INTEGER
@@ -133,15 +208,137 @@ public class NuiWrapper
         [MarshalAs(UnmanagedType.ByValArray, SizeConst=6)]
         public NuiSkeletonData[] SkeletonData;
     }
-	
+
+    public enum NuiImageDigitalzoom
+    {
+        x1 = 0,
+    } 
+
+    public struct NuiImageViewArea 
+    {
+        NuiImageDigitalzoom DigitalZoom;
+        UInt32 CenterX;
+        UInt32 CenterY;
+    };
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct NuiImageFrame 
+    {
+        public LARGE_INTEGER TimeStamp;
+        public UInt32 FrameNumber;
+        public NuiImageType ImageType;
+        public NuiImageResolution Resolution;
+        public IntPtr FrameTexture;
+        public UInt32 FrameFlags;
+        public NuiImageViewArea ViewArea;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct NuiLockedRect 
+    {
+        public int Pitch;
+        public int Size;
+        public IntPtr ActualDataFinally;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ColorPixel
+    {
+        public byte b;
+        public byte g;
+        public byte r;
+        public byte a;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ColorBuffer
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 640 * 480)]
+        public ColorPixel[] data;
+    }
+
+    public class INuiFrameTexture
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct INuiFrameTextureVtbl
+        {
+            public IntPtr QueryInterface;
+            public IntPtr AddRef;
+            public IntPtr Release;
+
+            public IntPtr BufferLen;
+            public IntPtr Pitch;
+
+            public IntPtr LockRect;
+            public IntPtr GetLevelDesc;
+            public IntPtr UnlockRect;
+        }
+
+        INuiFrameTextureVtbl vtbl;
+        IntPtr p;
+
+        delegate int BufferLenDelegate(IntPtr This);
+        delegate int PitchDelegate(IntPtr This);
+        delegate UInt32 LockRectDelegate(IntPtr This, uint level, ref NuiLockedRect rect, IntPtr pRect, UInt32 Flags);
+        delegate UInt32 UnlockRectDelegate(IntPtr This, uint level);
+
+        BufferLenDelegate bufferLen;
+        PitchDelegate pitch;
+        LockRectDelegate lockRect;
+        UnlockRectDelegate unlockRect;
+
+        public INuiFrameTexture(IntPtr p) {
+            this.p = p;
+
+            IntPtr pVtbl = (IntPtr)Marshal.PtrToStructure(p, typeof(IntPtr));
+            vtbl = (INuiFrameTextureVtbl)Marshal.PtrToStructure(pVtbl, typeof(INuiFrameTextureVtbl));
+
+            bufferLen = (BufferLenDelegate)Marshal.GetDelegateForFunctionPointer(vtbl.BufferLen, typeof(BufferLenDelegate));
+            pitch = (PitchDelegate)Marshal.GetDelegateForFunctionPointer(vtbl.Pitch, typeof(PitchDelegate));
+            lockRect = (LockRectDelegate)Marshal.GetDelegateForFunctionPointer(vtbl.LockRect, typeof(LockRectDelegate));
+            unlockRect = (UnlockRectDelegate)Marshal.GetDelegateForFunctionPointer(vtbl.UnlockRect, typeof(UnlockRectDelegate));
+        }
+
+        public int Pitch {
+            get {
+                return pitch(p);
+            }
+        }
+
+        public int BufferLen {
+            get {
+                return bufferLen(p);
+            }
+        }
+
+        public NuiLockedRect LockRect() {
+            NuiLockedRect result = new NuiLockedRect();
+            lockRect(p, 0, ref result, IntPtr.Zero, 0);
+            return result;
+        }
+
+        public void UnlockRect() {
+            unlockRect(p, 0);
+        }
+    }
+
 	//-------------------------------------------------------------------------
 	
 	[DllImport("kinect10.dll")]
 	public static extern UInt32 NuiInitialize(UInt32 Flags);
-	
+
 	[DllImport("kinect10.dll")]
 	public static extern UInt32 NuiSkeletonGetNextFrame(UInt32 Milliseconds, ref NuiSkeletonFrame skeletonFrame);
 	
+    [DllImport("kinect10.dll")]
+	public static extern UInt32 NuiImageStreamOpen(NuiImageType ImageType, NuiImageResolution Resolution, UInt32 ImageFrameFlags, UInt32 FrameLimit, IntPtr NextFrameEvent, out IntPtr StreamHandle);
+
+    [DllImport("kinect10.dll")]
+    public static extern UInt32 NuiImageStreamGetNextFrame(IntPtr Stream, UInt32 MillisecondsToWait, out IntPtr ImageFrame);
+
+    [DllImport("kinect10.dll")]
+    public static extern UInt32 NuiImageStreamReleaseFrame(IntPtr Stream, ref NuiImageFrame ImageFrame);
+
 	[DllImport("kinect10.dll")]
 	public static extern void NuiShutdown();
 }
@@ -149,25 +346,79 @@ public class NuiWrapper
 public class ZigInputKinectSDK : IZigInputReader
 {
 	NuiWrapper.NuiSkeletonFrame skeletonFrame = new NuiWrapper.NuiSkeletonFrame();
-	
+    NuiWrapper.NuiImageFrame depthFrame;
+    NuiWrapper.NuiImageFrame imageFrame;
+    NuiContext context;
+
+    short[] rawDepthMap;
+    float[] depthHistogramMap;
+    Color32[] depthMapPixels;
+    int XRes;
+    int YRes;
+    int factor;
+
+    int ImageXRes;
+    int ImageYRes;
+    Color32[] rawImageMap;
+
+    int lastDepthFrameId;
+    int lastImageFrameId;
+
 	//-------------------------------------------------------------------------
 	// IZigInputReader interface
 	//-------------------------------------------------------------------------
-	
+
 	public void Init()
 	{
 		UInt32 flags = 
 			(uint)NuiWrapper.NuiInitializeFlag.UsesDepthAndPlayerIndex | 
-		    (uint)NuiWrapper.NuiInitializeFlag.UsesSkeleton;
-		
-		if (PreventDoubleInit.IsSafeToInit()) {
-			UInt32 hr = NuiWrapper.NuiInitialize(flags);
-			if (0 != hr) {
-				throw new Exception("Error initing Kinect SDK: " + hr);
-			}
+		    (uint)NuiWrapper.NuiInitializeFlag.UsesSkeleton;// |
+            //(uint)NuiWrapper.NuiInitializeFlag.UsesColor;
+        
+        if (PreventDoubleInit.IsSafeToInit()) {
 
-			PreventDoubleInit.MarkInited();
-		}
+            UInt32 hr = NuiWrapper.NuiInitialize(flags);
+            if (0 != hr) {
+                throw new Exception("Error initing Kinect SDK: " + hr);
+            }
+
+            context = new NuiContext();
+            hr = NuiWrapper.NuiImageStreamOpen(NuiWrapper.NuiImageType.DepthAndPlayerIndex, NuiWrapper.NuiImageResolution.Res320x240, 0, 2, IntPtr.Zero, out context.DepthHandle);
+            if (0 != hr) {
+                throw new Exception("Error opening depth stream: " + hr);
+            }
+
+            //hr = NuiWrapper.NuiImageStreamOpen(NuiWrapper.NuiImageType.Color, NuiWrapper.NuiImageResolution.Res640x480, 0, 2, IntPtr.Zero, out context.ImageHandle);
+            //if (0 != hr) {
+            //    throw new Exception("Error opening image stream: " + hr);
+            //}
+            
+            PreventDoubleInit.SaveContext<NuiContext>(context);
+            PreventDoubleInit.MarkInited();
+        }
+        else {
+            context = PreventDoubleInit.LoadContext<NuiContext>();
+        }
+
+        // init textures
+        factor = 1;
+        XRes = 320 / factor;
+        YRes = 240 / factor;
+        Depth = new Texture2D(XRes, YRes);
+
+        //ImageXRes = 640 / factor;
+        //ImageYRes = 480 / factor;
+        //Image = new Texture2D(ImageXRes, ImageYRes);
+        //rawImageMap = new Color32[ImageXRes * ImageYRes];
+
+        // depthmap data
+        rawDepthMap = new short[XRes * YRes];
+        depthMapPixels = new Color32[(XRes / factor) * (YRes / factor)];
+
+        // histogram stuff
+        int maxDepth = 4000;
+        depthHistogramMap = new float[maxDepth];
+
 	}
 	
 	public void Update() 
@@ -175,6 +426,43 @@ public class ZigInputKinectSDK : IZigInputReader
 		if (0 == NuiWrapper.NuiSkeletonGetNextFrame(0, ref skeletonFrame)) {
 			ProcessNewSkeletonFrame();
 		}
+        IntPtr pDepthFrame;
+        if (0 == NuiWrapper.NuiImageStreamGetNextFrame(context.DepthHandle, 0, out pDepthFrame)) {
+            // deal with deref'ing/marshalling
+            depthFrame = (NuiWrapper.NuiImageFrame) Marshal.PtrToStructure(pDepthFrame, typeof(NuiWrapper.NuiImageFrame));
+            NuiWrapper.INuiFrameTexture depthTexture = new NuiWrapper.INuiFrameTexture(depthFrame.FrameTexture);
+
+            // lock & copy the depth data
+            NuiWrapper.NuiLockedRect rect = depthTexture.LockRect();
+            Marshal.Copy(rect.ActualDataFinally, rawDepthMap, 0, rawDepthMap.Length);
+            depthTexture.UnlockRect();
+
+            // process it
+            UpdateHistogram();
+            UpdateDepthmapTexture();
+            
+            // release current frame
+            NuiWrapper.NuiImageStreamReleaseFrame(context.DepthHandle, ref depthFrame);
+        }
+
+        //IntPtr pImageFrame;
+        //if (0 == NuiWrapper.NuiImageStreamGetNextFrame(context.ImageHandle, 0, out pImageFrame)) {
+        //    imageFrame = (NuiWrapper.NuiImageFrame)Marshal.PtrToStructure(pImageFrame, typeof(NuiWrapper.NuiImageFrame));
+        //    Debug.Log("Image frame: " + imageFrame.FrameNumber);
+            //NuiWrapper.INuiFrameTexture imageTexture = new NuiWrapper.INuiFrameTexture(imageFrame.FrameTexture);
+            /*
+            NuiWrapper.NuiLockedRect rect = imageTexture.LockRect();
+            NuiWrapper.ColorBuffer colors = (NuiWrapper.ColorBuffer)Marshal.PtrToStructure(rect.ActualDataFinally, typeof(NuiWrapper.ColorBuffer));
+            for (int i = 0; i < rect.Size; i++) {
+                rawImageMap[i].r = colors.data[i].r;
+                rawImageMap[i].g = colors.data[i].g;
+                rawImageMap[i].b = colors.data[i].b;
+                rawImageMap[i].a = colors.data[i].a;
+            }
+            imageTexture.UnlockRect();*/
+
+       //     NuiWrapper.NuiImageStreamReleaseFrame(context.ImageHandle, ref imageFrame);
+        //}
 	}
 	
 	public void Shutdown() {
@@ -282,7 +570,6 @@ public class ZigInputKinectSDK : IZigInputReader
 
 	private Vector3 jointPositionFromSkeleton(NuiWrapper.NuiSkeletonData skeleton, NuiWrapper.NuiSkeletonPositionIndex index)
 	{
-		//return Vector4ToVector3(skeleton.SkeletonPositions[(int)index]);
 		return vec4to3(skeleton.SkeletonPositions[(int)index]);
 	}
 
@@ -312,8 +599,6 @@ public class ZigInputKinectSDK : IZigInputReader
 			Vector3 worldY = new Vector3(col1.x, col1.y, -col1.z);
             Vector3 worldZ = new Vector3(-col2.x, -col2.y, col2.z);
 			return Quaternion.LookRotation(worldZ, worldY);
-			//Quaternion rot = Quaternion.LookRotation(col2, col1);
-			//return rot;
 		}
 	}		
 
@@ -415,7 +700,6 @@ public class ZigInputKinectSDK : IZigInputReader
 	   
 			case NuiWrapper.NuiSkeletonPositionIndex.ShoulderLeft:
 				result = orientationFromXY(
-					//vectorBetweenNuiJoints(ref skeleton,NuiWrapper.NuiSkeletonPositionIndex.ShoulderLeft,	NuiWrapper.NuiSkeletonPositionIndex.ElbowLeft),
 			        -vectorBetweenNuiJoints(ref skeleton,NuiWrapper.NuiSkeletonPositionIndex.ShoulderLeft,	NuiWrapper.NuiSkeletonPositionIndex.ElbowLeft),
 					vectorBetweenNuiJoints(ref skeleton,NuiWrapper.NuiSkeletonPositionIndex.ElbowLeft,		NuiWrapper.NuiSkeletonPositionIndex.WristLeft));
 				break;
@@ -503,8 +787,58 @@ public class ZigInputKinectSDK : IZigInputReader
 				break;
 		}
 		
-        // turn result into arraylist and pass it like a kidney stone
         return result.ToQuaternion();
 	}
+
+    void UpdateHistogram() {
+        int i, numOfPoints = 0;
+
+        Array.Clear(depthHistogramMap, 0, depthHistogramMap.Length);
+
+        short curr;
+        for (i = 0; i < rawDepthMap.Length; i++) {
+            // only calculate for valid depth
+            curr = (short)(rawDepthMap[i] >> 3);
+            if (curr != 0) {
+                depthHistogramMap[curr]++;
+                numOfPoints++;
+            }
+        }
+
+        if (numOfPoints > 0) {
+            for (i = 1; i < depthHistogramMap.Length; i++) {
+                depthHistogramMap[i] += depthHistogramMap[i - 1];
+            }
+            for (i = 0; i < depthHistogramMap.Length; i++) {
+                depthHistogramMap[i] = (1.0f - (depthHistogramMap[i] / numOfPoints)) * 255;
+            }
+        }
+        depthHistogramMap[0] = 0;
+    }
+
+    void UpdateDepthmapTexture() {
+        // flip the depthmap as we create the texture
+        int YScaled = YRes / factor;
+        int XScaled = XRes / factor;
+        int i = XScaled * YScaled - XScaled;
+        int depthIndex = 0;
+        for (int y = 0; y < YScaled; ++y, i -= XScaled) {
+            for (int x = 0; x < XScaled; ++x, depthIndex += factor) {
+                short pixel = (short)(rawDepthMap[depthIndex] >> 3);
+                if (pixel == 0) {
+                    depthMapPixels[i + x] = Color.clear;
+                }
+                else {
+                    Color32 c = new Color32((byte)depthHistogramMap[pixel], (byte)depthHistogramMap[pixel], 0, 255);
+                    depthMapPixels[i + x] = c;
+                }
+            }
+            // Skip lines
+            depthIndex += (factor - 1) * XRes;
+        }
+
+        Depth.SetPixels32(depthMapPixels);
+        Depth.Apply();
+    }
 }
 
