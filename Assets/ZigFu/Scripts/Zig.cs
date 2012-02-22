ba47579bd67c0b8d98c2698de72f397d44539467
shlomo.zippel <shlomo@zigfu.com>
2012-02-22 14:01:13 -0800

    moved some files around

--- Assets/ZigFu/Scripts/Zig.cs
+++ Assets/ZigFu/Scripts/Zig.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Zig : MonoBehaviour {
     public ZigInputType inputType = ZigInputType.OpenNI;
     public bool UpdateDepthmapImage = false;
     public bool UpdateImagemapImage = false;
 
-	void Awake () {
+    public List<GameObject> listeners = new List<GameObject>();
+
+	void Start () {
         #if UNITY_WEBPLAYER
         #if UNITY_EDITOR
         Debug.LogError("Depth camera input will not work in editor when target platform is Webplayer. Please change target platform to PC/Mac standalone.");
@@ -19,4 +22,21 @@ public class Zig : MonoBehaviour {
         ZigInput.UpdateImage = UpdateImagemapImage;
         ZigInput.Instance.AddListener(gameObject);
 	}
+
+    void notifyListeners(string msgname, object arg) {
+        for (int i = 0; i < listeners.Count; ) {
+            GameObject go = listeners[i];
+            if (go) {
+                go.SendMessage(msgname, arg, SendMessageOptions.DontRequireReceiver);
+                i++;
+            }
+            else {
+                listeners.RemoveAt(i);
+            }
+        }
+    }
+
+    void Zig_NewUser(ZigTrackedUser user) {
+        notifyListeners("Zig_NewUser", user);
+    }
 }
