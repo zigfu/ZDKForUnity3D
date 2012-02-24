6b6bd68f4e0d99633edd5bf20d5c15e63e53f258
shlomo.zippel <shlomo@zigfu.com>
2012-02-23 18:30:50 -0800

    updated based on zigjs

--- Assets/ZigFu/Scripts/Controls/ZigFader.cs
+++ Assets/ZigFu/Scripts/Controls/ZigFader.cs
@@ -5,95 +5,96 @@ public class ZigFader : MonoBehaviour {
 	public Vector3 direction = Vector3.right;
 	public float size = 200;
     public float initialValue = 0.5f;
+    public int itemCount = 0;
+    public float hysteresis = 0.2f;
 
-    public float value { get; private set; }
-
-    Vector3 mirrorIndependentDirection {
-        get {
-            //return (OpenNIContext.Instance.Mirror) ? direction : Vector3.Scale(direction, new Vector3(-1, 1, 1));
-			return direction; // assumes mirror is always on
-        }
-    }
+    // these should be private set but this way they're visible in the inspector
+    public float value; // { get; private set; }
+    public int hoverItem = -1; // { get; private set; }
 
     Vector3 start;
-	
+    bool isEdge;
+
+    void Start() {
+        value = initialValue;
+    }
+
 	// move the slider to contain pos within its bounds
-	public void MoveToContain(Vector3 pos)
-	{
-		float dot = Vector3.Dot(mirrorIndependentDirection, pos - start);
+	public void MoveToContain(Vector3 pos) {
+		float dot = Vector3.Dot(direction, pos - start);
 		if (dot > size) {
-			start += mirrorIndependentDirection * (dot - size);
+            start += direction * (dot - size);
 		}
 		if (dot < 0) {
-			start += mirrorIndependentDirection * dot;
+            start += direction * dot;
 		}
 	}
 	
 	// move the slider so that pos will be mapped to val (0-1)
-	public void MoveTo(Vector3 pos, float val)
-	{
-        start = pos - (mirrorIndependentDirection * (val * size));
+	public void MoveTo(Vector3 pos, float val) {
+        start = pos - (direction * (val * size));
 	}
 	
-	public void ForceUpdate(Vector3 pos)
-	{
-		value = GetValue(pos);
+	public void UpdatePosition(Vector3 pos) {
+        UpdateValue(GetValue(pos));
 	}
 	
-	public float GetValue(Vector3 pos)
-	{
-		float dot = Vector3.Dot(mirrorIndependentDirection, pos - start);
+	public float GetValue(Vector3 pos) {
+		float dot = Vector3.Dot(direction, pos - start);
         float val = Mathf.Clamp01(dot / size);
         return val;
     }
-	
-	public Vector3 GetPosition(float val)
-	{
-        return start + (mirrorIndependentDirection * (val * size));
-	}
-	
-	bool InSession;
-	
-	/*
-    // hand point control messages
-	
-    void Zig_OnSessionStart(ZigEventArgs args)
-    {
-        MoveTo(args.sender.FocusPoint, initialValue);
-        value = initialValue;
-		InSession = true;
-    }
 
-    void Zig_OnSessionUpdate(ZigEventArgs args)
-    {
-        value = GetValue(args.HandPosition);
-    }
+    public void UpdateValue(float val) {
+        this.value = val;
+    
+        // value change
+        SendMessage("Fader_ValueChange", this, SendMessageOptions.DontRequireReceiver);
 
-    void Zig_OnSessionEnd()
-    {
-        value = initialValue;
-		InSession = false;
-    }*/
+        // edge
+        bool isEdgeThisFrame = Mathf.Approximately(val, 0) || Mathf.Approximately(val, 1.0f);
+        if (!isEdge && isEdgeThisFrame) {
+            SendMessage("Fader_Edge", this, SendMessageOptions.DontRequireReceiver);
+        }
+        isEdgeThisFrame = isEdge;
 
-    void Start()
-    {
-        value = initialValue;
-    }
-	
-	void Zig_Visualize()
-	{
-		
-	}
-	
-	void OnGUI()
-	{
-		GUILayout.BeginVertical("box");
-		GUILayout.Label("Fader:");
-		if (InSession) {
-			GUILayout.HorizontalSlider(value, 0, 1);
-		} else {
-			GUILayout.Label("Not in session");
+        // item hover
+   		int newHover = hoverItem;
+		float minValue = (hoverItem * (1 / itemCount)) - hysteresis;
+		float maxValue = (hoverItem + 1) * (1 / itemCount) + hysteresis;
+        if (value > maxValue) {
+			newHover++;
 		}
-		GUILayout.EndVertical();
+		if (value < minValue) {
+			newHover--;
+		}
+		
+		if (newHover != hoverItem) {
+            if (hoverItem != -1) SendMessage("Fader_HoverStop", this, SendMessageOptions.DontRequireReceiver);
+            hoverItem = newHover;
+			SendMessage("Fader_HoverStart", this, SendMessageOptions.DontRequireReceiver);
+		}
+    }
+	
+	public Vector3 GetPosition(float val) {
+        return start + (direction * (val * size));
 	}
+
+    // hand point session messages
+	
+    void Session_Start(Vector3 focusPosition)
+    {
+        MoveTo(focusPosition, initialValue);
+        value = initialValue;
+    }
+
+    void Session_Update(Vector3 handPosition)
+    {
+        UpdatePosition(handPosition);
+    }
+
+    void Session_End()
+    {
+        value = initialValue;
+    }
 }
