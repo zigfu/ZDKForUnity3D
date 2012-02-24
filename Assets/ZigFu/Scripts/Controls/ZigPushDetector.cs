79213635427b3113538959e120f4f6a286f39a08
shlomo.zippel <shlomo@zigfu.com>
2012-02-23 18:31:09 -0800

    updated pushdetecto to zigjs

--- Assets/ZigFu/Scripts/Controls/ZigPushDetector.cs
+++ Assets/ZigFu/Scripts/Controls/ZigPushDetector.cs
@@ -7,7 +7,7 @@ public class ZigPushDetector : MonoBehaviour {
 	public float driftSpeed = 0.05f;
 	
 	public float clickTimeFrame = 1.0f;
-    public float clickMaxDistance = 100; //??
+    public float clickMaxDistance = 100;
 
     public float clickPushTime { get; private set; }
 	public bool IsClicked { get; private set; }
@@ -20,72 +20,59 @@ public class ZigPushDetector : MonoBehaviour {
 	
     ZigFader pushFader;
 
-	void Start()
+    void Start()
 	{
         pushFader = gameObject.AddComponent<ZigFader>();
         pushFader.direction = Vector3.forward;
 	}
 
-	/*
-	void Zig_OnSessionStart(ZigEventArgs args)
-	{
+	void Session_Start(Vector3 focusPosition) {
         pushFader.size = size;
         pushFader.initialValue = initialValue;
-        pushFader.MoveTo(args.FocusPoint, initialValue);
+        pushFader.MoveTo(focusPosition, initialValue);
 	}
 	
-	bool sent_push;
-	void Zig_OnSessionUpdate(ZigEventArgs args)
+	void Session_Update(Vector3 handPosition)
 	{
-		Vector3 pos = args.HandPosition;
-		
 		// move slider if hand is out of its bounds (that way it always feels responsive)
-		pushFader.MoveToContain(pos);
-        pushFader.ForceUpdate(pos);
+		pushFader.MoveToContain(handPosition);
+        pushFader.UpdatePosition(handPosition);
 	
 		// click logic
 		if (!IsClicked) {
             if (ClickProgress == 1.0f) {
-				ClickPosition = pos;
+				ClickPosition = handPosition;
                 IsClicked = true;
 				clickPushTime = Time.time;
-				//SendMessage("PushDetector_Push", SendMessageOptions.DontRequireReceiver);
+				SendMessage("PushDetector_Push", SendMessageOptions.DontRequireReceiver);
             }
         }
         else { // clicked
            if (ClickProgress < 0.5) {
-                if (IsClick(clickPushTime, ClickPosition, Time.time, pos)) {
+                if (IsClick(clickPushTime, ClickPosition, Time.time, handPosition)) {
 					SendMessage("PushDetector_Click",SendMessageOptions.DontRequireReceiver);
 				}
 				
 				SendMessage("PushDetector_Release", SendMessageOptions.DontRequireReceiver);
                 IsClicked = false;
-				sent_push = false;
-            } else {
-				if (!sent_push && !IsClick(clickPushTime, ClickPosition, Time.time, pos)) {
-					SendMessage("PushDetector_Push", SendMessageOptions.DontRequireReceiver);
-					sent_push = true;
-				}
-			}
+            }
         }
 		
 		// drift the slider to the initial position, if we aren't clicked
 		if (!IsClicked) {
 			float delta = initialValue - ClickProgress;
-            pushFader.MoveTo(pos, ClickProgress + (delta * driftSpeed));
+            pushFader.MoveTo(handPosition, ClickProgress + (delta * driftSpeed));
 		}	
 	}
 	
-	void Zig_OnSessionEnd()
-	{
+	void Session_End() {
 		if (IsClicked) {
 			SendMessage("PushDetector_Release", SendMessageOptions.DontRequireReceiver);
 			IsClicked = false;
 		}
-	}*/
+	}
 	
-    bool IsClick(float t1, Vector3 p1, float t2, Vector3 p2)
-    {
+    bool IsClick(float t1, Vector3 p1, float t2, Vector3 p2) {
         Vector3 delta = (p2 - p1);
         delta.z = 0;
         return ((t2 - t1 < clickTimeFrame) && (delta.magnitude < clickMaxDistance));
