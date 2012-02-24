e8c1c021dedbbb1ddd88be28956ae311c7930fa3
shlomo.zippel <shlomo@zigfu.com>
2012-02-23 18:31:27 -0800

    wave detector UNTESTED

---
+++ Assets/ZigFu/Scripts/Controls/ZigWaveDetector.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class ZigWaveDetector : MonoBehaviour {
+    public int Waves = 5;
+    ZigFader waveFader;
+    List<float> timestampBuffer;
+
+    float lastEdge;
+
+    void Awake() {
+        waveFader = gameObject.AddComponent<ZigFader>();
+        // TODO: Init fader with drift, size
+    }
+
+    void Fader_Edge(ZigFader f) {
+        if (f != waveFader) return;
+        
+        // prune
+        while (timestampBuffer.Count > 0 && (Time.time - timestampBuffer[0] > 2.0f)) {
+            timestampBuffer.RemoveAt(0);
+        }
+
+        if (timestampBuffer.Count == 0) {
+            lastEdge = -1;
+        }
+
+        if (!Mathf.Approximately(lastEdge, f.value)) {
+            timestampBuffer.Add(Time.time);
+        }
+
+        lastEdge = f.value;
+        if (timestampBuffer.Count >= Waves) {
+            SendMessage("WaveDetector_Wave", this, SendMessageOptions.DontRequireReceiver);
+            timestampBuffer.Clear();
+        }
+    }
+}
