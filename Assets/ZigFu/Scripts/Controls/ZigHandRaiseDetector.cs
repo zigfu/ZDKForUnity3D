ff289c194bb7a850cd66aaa5668673eb0d23ae8d
Roee Shenberg <roee@zigfu.com>
2012-02-28 16:58:04 -0800

    initial hand-raise detector.
    It works by looking at the angle between the elbow-to-hand vector and the torso-to-neck vector whenever a hand is steady

--- Assets/ZigFu/Scripts/Controls/ZigHandRaiseDetector.cs
+++ Assets/ZigFu/Scripts/Controls/ZigHandRaiseDetector.cs
@@ -1,39 +1,91 @@
 using UnityEngine;
 using System.Collections;
+using System;
 //using OpenNI; // OpenNI dep for SkeletonJoint, will go soon
+public class HandRaiseEventArgs : EventArgs
+{
+    public ZigJointId Joint { get; private set; }
+    public HandRaiseEventArgs(ZigJointId joint)
+    {
+        Joint = joint;
+    }
+}
+
 
 public class ZigHandRaiseDetector : MonoBehaviour {
 	
 	ZigSteadyDetector leftHandSteady;
 	ZigSteadyDetector rightHandSteady;
-	//ZigTrackedUser user;
-	
-	// Use this for initialization
-	void Start () {
-		leftHandSteady = gameObject.AddComponent<ZigSteadyDetector>();
-		rightHandSteady = gameObject.AddComponent<ZigSteadyDetector>();
-		/*
-		leftHandSteady.type = SteadyDetectorType.SkeletonJoint;
-		leftHandSteady.joint = ZigJointId.LeftHand;
-		
-		rightHandSteady.type = SteadyDetectorType.SkeletonJoint;
-        rightHandSteady.joint = ZigJointId.RightHand;*/
+    GameObject leftHandDetector;
+    GameObject rightHandDetector;
+	ZigTrackedUser trackedUser;
+    public float angleThreshold = 30; // degrees
+
+    public event EventHandler<HandRaiseEventArgs> HandRaise;
+    protected void OnHandRaise(ZigJointId joint)
+    {
+       if (null != HandRaise) {
+            HandRaise.Invoke(this, new HandRaiseEventArgs(joint));
+        }
+       SendMessage("HandRaiseDetector_HandRaise", joint, SendMessageOptions.DontRequireReceiver);
+    }
+
+    // Use this for initialization
+	void Awake () {
+        leftHandDetector = new GameObject("LeftHandDetector");
+        leftHandDetector.transform.parent = gameObject.transform;
+        ZigMapJointToSession leftMap = leftHandDetector.AddComponent<ZigMapJointToSession>();
+        leftMap.joint = ZigJointId.LeftHand;
+
+        rightHandDetector = new GameObject("RightHandDetector");
+        rightHandDetector.transform.parent = gameObject.transform;
+        ZigMapJointToSession rightMap = rightHandDetector.AddComponent<ZigMapJointToSession>();
+        rightMap.joint = ZigJointId.RightHand;
+
+		leftHandSteady = leftHandDetector.AddComponent<ZigSteadyDetector>();
+		rightHandSteady = rightHandDetector.AddComponent<ZigSteadyDetector>();
+        leftHandSteady.Steady += delegate(object sender, EventArgs e) {
+            ZigInputJoint hand = trackedUser.Skeleton[(int)ZigJointId.LeftHand];
+            ZigInputJoint elbow = trackedUser.Skeleton[(int)ZigJointId.LeftElbow];
+            if (IsHandRaise(hand.Position, elbow.Position)) {
+                OnHandRaise(ZigJointId.LeftHand);
+            }
+        };
+        rightHandSteady.Steady += delegate(object sender, EventArgs e) {
+            ZigInputJoint hand = trackedUser.Skeleton[(int)ZigJointId.RightHand];
+            ZigInputJoint elbow = trackedUser.Skeleton[(int)ZigJointId.RightElbow];
+            if (IsHandRaise(hand.Position, elbow.Position)) {
+                OnHandRaise(ZigJointId.RightHand);
+            }
+        }; 
 	}
 	
-	/*
-	void Zig_OnUserUpdate(ZigEventArgs args)
-	{
-		user = args.user;
-	}*/
-	/*
-	void SteadyDetector_Steady(SkeletonJoint joint)
-	{
-		// if the steady point is a hand, and its higher than the head
-		if (SkeletonJoint.LeftHand == joint || SkeletonJoint.RightHand == joint) {
-			Vector3 head = user.Joints[SkeletonJoint.Head].position;
-			if (user.Joints[joint].position.y > head.y) {
-				SendMessage("HandRaiseDetector_HandRaised", user, SendMessageOptions.DontRequireReceiver);
-			}
-		}
-	}*/
+    void Zig_Attach(ZigTrackedUser user)
+    {
+        trackedUser = user;
+        user.AddListener(leftHandDetector);
+        user.AddListener(rightHandDetector);
+    }
+
+    void Zig_UpdateUser(ZigTrackedUser user)
+    {
+        trackedUser = user;
+    }
+
+    void Zig_Detach(ZigTrackedUser user)
+    {
+        user.RemoveListener(leftHandDetector);
+        user.RemoveListener(rightHandDetector);
+        trackedUser = null;
+    }
+    bool IsHandRaise(Vector3 handPosition, Vector3 elbowPosition)
+    {
+        ZigInputJoint torso = trackedUser.Skeleton[(int)ZigJointId.Torso];
+        ZigInputJoint neck = trackedUser.Skeleton[(int)ZigJointId.Neck];
+
+        Vector3 armDirection = (handPosition - elbowPosition).normalized;
+        Vector3 torsoDirection = (neck.Position - torso.Position).normalized;
+        double angle = Math.Acos(Vector3.Dot(armDirection, torsoDirection) * 180 / Math.PI);
+        return (angle < angleThreshold);
+    }
 }
