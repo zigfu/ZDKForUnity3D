ac0da749330b70695e9435d4a132c352e54dd71f
shlomo.zippel <shlomo@zigfu.com>
2012-02-29 16:24:43 -0800

    updated to reflect latest event names

--- Assets/ZigFu/Scripts/UserEngagers/ZigEngageAllUsers.cs
+++ Assets/ZigFu/Scripts/UserEngagers/ZigEngageAllUsers.cs
@@ -7,14 +7,14 @@ public class ZigEngageAllUsers : MonoBehaviour {
 	public GameObject InstantiatePerUser;
 	Dictionary<int, GameObject> objects = new Dictionary<int, GameObject>();
 	
-	void Zig_NewUser(ZigTrackedUser user) 
+	void Zig_UserFound(ZigTrackedUser user) 
 	{
 		GameObject o = Instantiate(InstantiatePerUser) as GameObject;
 		objects[user.Id] = o;
 		user.AddListener(o);
 	}
 	
-	void Zig_LostUser(ZigTrackedUser user)
+	void Zig_UserLost(ZigTrackedUser user)
 	{
 		Destroy(objects[user.Id]);
 		objects.Remove(user.Id);
