fa201770b6686b74c6977bf94fd1dd16e654623e
ckyun777 <ckyun777@gmail.com>
2013-07-21 03:12:01 +0900

    Update KinectGrisp170_CLR20.dll
    Update dll and Unity-dll interface C# code

--- Assets/ZigFu/Scripts/_Internal/MS_HandGrip/ZigMSHandGripTest.cs
+++ Assets/ZigFu/Scripts/_Internal/MS_HandGrip/ZigMSHandGripTest.cs
@@ -3,14 +3,17 @@ using System.Collections;
 
 using System.Runtime.InteropServices;
 using System.Text;
+using System;
+
+public enum GripHandState { NONE, GRIP, RELEASED };
 
 public class ZigMSHandGripTest : MonoBehaviour {
 
     public TextMesh m_MsgTxt;
-    public StringBuilder m_sbTxt;
-
-    [DllImport("KinectGrip170_CLR20", EntryPoint = "TestFunc")]
-    public static extern int TestFunc();
+    private StringBuilder m_SBTxt;
+    
+    public GripHandState m_RHandStat;
+    public GripHandState m_LHandStat;
 
 
     [DllImport("KinectGrip170_CLR20", EntryPoint = "InitKinectInteraction")]
@@ -25,42 +28,46 @@ public class ZigMSHandGripTest : MonoBehaviour {
     [DllImport("KinectGrip170_CLR20", EntryPoint = "GetRHandStat")]
     public static extern int GetRHandStat();
 
-    [DllImport("KinectGrip170_CLR20", EntryPoint = "KinectDataProc")]
+    [DllImport("KinectGrip170_CLR20", EntryPoint = "UpdateKinectData")]
     public static extern int KinectDataProc();
 
+
+    
     // Use this for initialization
 	void Start () {
-        
-        print(" KinectGrip TestFunc = " + TestFunc());
-
+        // Initialize MS Kinect Interaction Stream
         InitKinectInteraction();
-        m_sbTxt = new StringBuilder(1000);
+
+        m_SBTxt = new StringBuilder(100);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+
+        // Update MS Kinect Interaction Stream data    
         KinectDataProc();
 
-        if (GetRHandStat() != 0)
+        m_LHandStat = (GripHandState)Enum.ToObject(typeof(GripHandState), GetLHandStat());
+        m_RHandStat = (GripHandState)Enum.ToObject(typeof(GripHandState), GetRHandStat());
+
+        //if (m_LHandStat != GripHandState.NONE)
         {
-            m_sbTxt.AppendFormat(" R {0} ", GetRHandStat());
-            print(m_sbTxt.ToString());
-            m_MsgTxt.text = m_sbTxt.ToString();
+            m_SBTxt.AppendFormat(" Left {0} ", m_LHandStat.ToString());
+            m_MsgTxt.text = m_SBTxt.ToString();
         }
 
-        if (GetLHandStat() != 0)
+        //if (m_RHandStat != GripHandState.NONE)
         {
-            m_sbTxt.AppendFormat(": L {0} ", GetLHandStat());
-            print(m_sbTxt.ToString());
-            m_MsgTxt.text = m_sbTxt.ToString();
+            m_SBTxt.AppendFormat(" : Right {0} ", m_RHandStat.ToString());
+            m_MsgTxt.text = m_SBTxt.ToString();
         }
 
-        m_sbTxt.Remove(0, m_sbTxt.Length);
+        m_SBTxt.Remove(0, m_SBTxt.Length);
 	}
 
     void OnApplicationQuit()
     {
+        // Close handles and release the Kinect Sensor
         FinishKinectInteraction();
     }
 }
